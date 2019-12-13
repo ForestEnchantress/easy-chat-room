32c5563f75c58963a9a84289f804b3ea1443e7d3
ForestEnchantress <42741272+ForestEnchantress@users.noreply.github.com>
2019-12-13 13:03:33 +0800

    Add files via upload

---
+++ code/head.h
@@ -0,0 +1,30 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_ 
+#define _GNU_SOURCE 1
+#include<sys/mman.h>
+#include<sys/poll.h>
+#include<sys/uio.h>
+#include<errno.h>
+#include<time.h>
+#include<sys/sem.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<sys/epoll.h>
+#include<pthread.h>
+#include<libgen.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdbool.h>
+#include<arpa/inet.h>
+#include<signal.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/sendfile.h>
+typedef struct sockaddr SA;
+#endif
